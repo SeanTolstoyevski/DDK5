043438bb6f9e0d6a1d5237d49ce39186e3c64002
SeanTolstoyevski <lostpars@outlook.com>
2022-03-30 07:02:48 +0300

    first

---
+++ DDK5QUESTIONS/Program.cs
@@ -0,0 +1,19 @@
+﻿#region triangle
+Console.WriteLine("Ucgenin tabanini giriniz: ");
+int taban = Convert.ToInt32(Console.ReadLine());
+int hiza, yildiz;
+for(int i = 1; i <= taban; i++)
+{
+    for (hiza = 1; hiza <= taban - i; hiza++)
+    {
+        Console.Write(" ");
+    }
+    for(yildiz = 1; yildiz <= i; yildiz++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine("");
+}
+
+#endregion
+
