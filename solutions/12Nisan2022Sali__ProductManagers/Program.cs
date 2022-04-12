20926f5f4db9575a0207b181a36eba1814774768
SeanTolstoyevski <lostpars@outlook.com>
2022-04-12 12:01:46 +0300

    Products - Yeni Sorgular

--- solutions/12Nisan2022Sali__ProductManagers/Program.cs
+++ solutions/12Nisan2022Sali__ProductManagers/Program.cs
@@ -27,6 +27,31 @@ namespace HelloCSharp
             Console.WriteLine($"Kategorisi 3 ve tedarikçi'si 2 olan {Category3AndSuplier2Count} var");
 
 
+            int Price50_100RangeCount = products.Where(v => v.unitPrice >= 50.0m && v.unitPrice <= 100.0m).ToList().Count();
+            Console.WriteLine($"Fiyati 50-100 arasinda {Price50_100RangeCount} urun var.");
+
+            Console.WriteLine("a harfi ile baslayan urunler:");
+            var fieeldedStartA = products.Where(v => v.name[0].ToString().ToLower() == "a").ToList();
+            foreach(var i in fieeldedStartA)
+                Console.WriteLine(i.name);
+
+            Console.WriteLine("a harfi ile biten urunler:");
+            var fieeldedEndA = products.Where(v => v.name[v.name.Length - 1].ToString().ToLower() == "a").ToList();
+            foreach(var i in fieeldedEndA )
+                Console.WriteLine(i.name);
+
+            Console.WriteLine("a harfi iceren urunler:");
+            var fieldedContainsALetter = products.Where(v => v.name.ToLower().Contains('a')).ToList();
+            foreach(var i in fieldedContainsALetter )
+                Console.WriteLine(i.name);
+
+            int lesStock10 = products.Where(v => v.unitsInStock < 10).ToList().Count();
+            Console.WriteLine($"Stok adedi 10'dan az olan urun sayisi {lesStock10}");
+
+
+
+
+
 
 
 
@@ -37,4 +62,6 @@ namespace HelloCSharp
 
     }
 }
+
+
 
