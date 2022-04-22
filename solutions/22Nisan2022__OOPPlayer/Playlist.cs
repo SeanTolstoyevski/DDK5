dfc180b77d938392aeda1d44b3bf232ea52d4273
Emre <seantolstoyevski@protonmail.com>
2022-04-22 10:23:01 +0300

    22 Nisan 2022 - OOP Player ISongEntity -> Playlist Implementasyonu

--- solutions/22Nisan2022__OOPPlayer/Playlist.cs
+++ solutions/22Nisan2022__OOPPlayer/Playlist.cs
@@ -2,11 +2,15 @@ using System.Collections.Generic;
 
 namespace Player;
 
-public class PlayList
+public class PlayList : ISongEntity
 {
+    
+    
     public string name { get; set; }
     public List<Song> songs { get;  set; }
+    public int SongCount { get => songs.Count(); }
     public User user { get; set; }
+    public DateTime LastPlayedDate { get; set; }
 
 }
 
