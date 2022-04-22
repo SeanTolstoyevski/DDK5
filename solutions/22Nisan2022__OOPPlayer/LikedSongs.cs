a95d6b2d19b47486343ece3997b3ea5f9b597a3d
Emre <seantolstoyevski@protonmail.com>
2022-04-22 09:55:46 +0300

    22 Nisan 2022 - OOp Player User ve LikedSongs Eklendi

---
+++ solutions/22Nisan2022__OOPPlayer/LikedSongs.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace Player;
+
+public class LikedSongs
+{
+    public List<Song> songs { get; set; }
+    public User user { get; set; }
+
+}
+
+
---
+++ solutions/22Nisan2022__OOPPlayer/Users.cs
@@ -0,0 +1,15 @@
+namespace Player;
+
+public class User
+{
+    public User(string name, string surname)
+    {
+        this.name = name;
+        this.surname = surname;
+    }
+    public string name { get; set; }
+    public string surname { get; set; }
+
+}
+
+
