23f2e0464a268014c617f0121f9c64ead451299d
Emre <seantolstoyevski@protonmail.com>
2022-04-25 14:05:31 +0300

    25 Nisan 2022 - IO Islemleri (parsing cities and countries)

---
+++ solutions/25Nisan2022__CityParser/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace cities;
+
+
+public class Country
+{
+    public string Name { get; set; }
+}
+
+
+public class City
+{
+    public Country country { get; set; }
+    public string CityName { get; set; }
+}
+
+public class cities
+{
+    public cities(string path)
+    {
+        string[] allFileContent = File.ReadAllLines(path);
+        foreach (var c in allFileContent)
+        {
+            var parsed = c.Split(", ");
+            var cty = new City();
+            cty.country = new Country();
+            cty.country.Name = parsed[1];
+            cty.CityName = parsed[0];
+            cityList.Add(cty);
+
+        }
+    }
+    public List<City> cityList = new List<City>();
+}
+
+class Program
+{
+    static void Main()
+    {
+        var collectionOfCity = new cities("cities.txt");
+        foreach (var c in collectionOfCity.cityList)
+        {
+            Console.WriteLine("City: {0} - Country: {1}", c.CityName, c.country.Name);
+        }
+    }
+}
+
