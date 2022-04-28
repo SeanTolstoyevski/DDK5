eb92c7c826c2180006dd3e1c8754c82511eb1a23
Emre <seantolstoyevski@protonmail.com>
2022-04-28 09:05:41 +0300

    27 Nisan 2022 - SQL Sorulari ve Cevaplari (Ilk 3)

--- solutions/27Nisan2022__sqlConnectionCSharp/Manager/ProductManager.cs
+++ solutions/27Nisan2022__sqlConnectionCSharp/Manager/ProductManager.cs
@@ -45,6 +45,80 @@ namespace SQLWorking.Manager
                 }
 
             }
+
         }
+
+
+        public List<Product> GetMinMaxProducts(decimal min, decimal max)
+        {
+                        List<Product> products = new List<Product>();
+
+            SqlConnection sqlConnection = new SqlConnection(CInfo.connectionString);
+                    sqlConnection.Open();
+                    SqlCommand cmd = new SqlCommand("select * from Products where UnitPrice > @minPrice and UnitPrice < @maxPrice", sqlConnection);
+                cmd.Parameters.AddWithValue("@minPrice", min);
+                cmd.Parameters.AddWithValue("@maxPrice", max);
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Product product = new Product();
+                        product.Id = Convert.ToInt32(reader["ProductId"]);
+                        product.Name = reader["ProductName"].ToString();
+                        product.UnitPrice = Convert.ToDecimal(reader["UnitPrice"]);
+                        product.UnitsInStock = Convert.ToInt32(reader["UnitsInStock"]);
+                        products.Add(product);
+                    }
+                    sqlConnection.Close();
+
+            return products;
+        }
+
+        public List<Product> GetStockZero()
+        {
+                        List<Product> products = new List<Product>();
+
+            SqlConnection sqlConnection = new SqlConnection(CInfo.connectionString);
+                    sqlConnection.Open();
+                    SqlCommand cmd = new SqlCommand("select * from Products where UnitsInStock = 0", sqlConnection);
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Product product = new Product();
+                        product.Id = Convert.ToInt32(reader["ProductId"]);
+                        product.Name = reader["ProductName"].ToString();
+                        product.UnitPrice = Convert.ToDecimal(reader["UnitPrice"]);
+                        product.UnitsInStock = Convert.ToInt32(reader["UnitsInStock"]);
+                        products.Add(product);
+                    }
+                    sqlConnection.Close();
+
+            return products;
+        }
+
+
+
+        public List<Product> SearchName(string name)
+        {
+                        List<Product> products = new List<Product>();
+            SqlConnection sqlConnection = new SqlConnection(CInfo.connectionString);
+                    sqlConnection.Open();
+                    SqlCommand cmd = new SqlCommand("select * from Products where ProductName like '%" + name + "%'", sqlConnection);
+
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Product product = new Product();
+                        product.Id = Convert.ToInt32(reader["ProductId"]);
+                        product.Name = reader["ProductName"].ToString();
+                        product.UnitPrice = Convert.ToDecimal(reader["UnitPrice"]);
+                        product.UnitsInStock = Convert.ToInt32(reader["UnitsInStock"]);
+                        products.Add(product);
+                    }
+                    sqlConnection.Close();
+
+            return products;
+        }
+
     }
 }
+
--- solutions/27Nisan2022__sqlConnectionCSharp/Program.cs
+++ solutions/27Nisan2022__sqlConnectionCSharp/Program.cs
@@ -8,9 +8,24 @@ class program
 {
     static void Main()
     {
-        var sManager = new SuplierManager();
-        var suplierList= sManager.GetAllSupliers();
+        var pManager = new ProductManager();
+var minMaxProducts = pManager .GetMinMaxProducts(5M, 25M);
+        foreach(var p in minMaxProducts)
+            Console.WriteLine("{0}, - {1}", p.Name, p.UnitPrice);
         
+        var zeroStocksProducts = pManager.GetStockZero();
+        foreach(var p in zeroStocksProducts)
+            Console.WriteLine("Zero Stock : {0}, - {1}", p.Name, p.UnitPrice);
+
+
+        Console.WriteLine("aramak istediginiz urun:");
+        string searchName = Console.ReadLine();
+        var searchedResults = pManager.SearchName(searchName);
+        foreach(var p in searchedResults)
+            Console.WriteLine("Name search: {0}", p.Name);
+
+        
+
 
 
     } // main block
