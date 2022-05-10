db813d4c73787b3f7a769043d950acefb0ea4786
Emre <seantolstoyevski@protonmail.com>
2022-05-10 10:59:06 +0300

    restructured

--- solutions/oop/oop1/Films.cs
+++
@@ -1,9 +0,0 @@
-namespace Oop1
-{
-    public class Film
-    {       
-        public string Name { get; set; }
-        public int Year { get; set; }
-        public double Rating { get; set; }
-    }
-}
