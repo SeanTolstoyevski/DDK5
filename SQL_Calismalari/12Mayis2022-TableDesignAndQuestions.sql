5d45c727b5cfafc580b29f2b326425709edcdb0d
Emre <seantolstoyevski@protonmail.com>
2022-05-12 14:42:20 +0300

    12 Mayis 2022 - Diger Sorularin Cevaplari

--- SQL_Calismalari/12Mayis2022-TableDesignAndQuestions.sql
+++ SQL_Calismalari/12Mayis2022-TableDesignAndQuestions.sql
@@ -33,11 +33,23 @@ create table AcademicianDepartment(
 )
 
 
-
 -- Öğrenci Ad, Soyad, Bölüm Ad, Fakülte Ad
 select s.name, s.lastName, (d.name) ['Department Name'], (f.name) ['Faculty Name'] from Student  s
 inner join Department d on d.id  = s.departmentID
 inner join Faculty f on f.id = d.facultyID
+
+
+-- Eğitmen Ad, Bölüm Ad
+select a.name, a.lastName, (d.name) ['Department Name] from AcademicianDepartment ad
+inner join Academician a on a.id = ad.AcademicianID
+inner join Department d on d.id = ad.departmentID
+
+
+-- Fen Fakültesindeki EĞİTMENLERİ LİSTELE
+select a.name, a.lastName  from AcademicianDepartment ad
+inner join Academician a on a.id = ad.academicianID
+inner join Department d on d.id = ad.departmentID
+inner join Faculty f on f.id = d.facultyID WHERE d.facultyID = 1
 
 
 
