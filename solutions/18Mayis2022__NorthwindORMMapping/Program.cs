0d947ceb6c1552a168985ce0366cd7d95f8d5e87
Emre <seantolstoyevski@protonmail.com>
2022-05-18 17:13:30 +0300

    18 Mayis 2022 Carsamba - Employee mapping

--- solutions/18Mayis2022__NorthwindORMMapping/Program.cs
+++ solutions/18Mayis2022__NorthwindORMMapping/Program.cs
@@ -171,6 +171,39 @@ public class Order
 
 }
 
+ public class Employee
+    {
+        [Required]
+        public int ID { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string LastName { get; set; }
+        [StringLength(10)]
+        public string FirstName { get; set; }
+        [StringLength(30)]
+        public string Title { get; set; }
+        [StringLength(25)]
+        public string TitleOfCourtesy { get; set; }
+        
+        public DateTime  BirthDate { get; set; }
+        public DateTime HireDate { get; set; }
+        [StringLength(60)]
+        public string Adress { get; set; }
+        [StringLength(15)]
+        public string City { get; set; }
+        [StringLength(15)]
+        public string Region { get; set; }
+        [StringLength(10)]
+        public string PostalCode { get; set; }
+        [StringLength(15)]
+        public string Country { get; set; }
+        [StringLength(24)]
+        public string HomePhone { get; set; }
+        [StringLength(4)]
+        public string Extension { get; set; }
+        public string Notes { get; set; }
+        public int ReportTo { get; set; }
+    }
 class program
 {
 
