326b039421b29f61f8093b2e1e097890d27dced3
Emre <seantolstoyevski@protonmail.com>
2022-05-21 09:01:59 +0300

    18 Mayis 2022 Carsamba - Northwind Mapping Migrations

---
+++ solutions/18Mayis2022__NorthwindORMMapping/Migrations/20220521055221_ApplyMigration.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _18Mayis2022__NorthwindORMMapping.Migrations
+{
+    public partial class ApplyMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerCustomerDemos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerID = table.Column<int>(type: "int", nullable: false),
+                    CustomerTypeID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerCustomerDemos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerDemographics",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", maxLength: 20, nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerDesc = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerDemographics", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyName = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    ContactName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    ContactTitle = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    City = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Region = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    PostalCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(24)", maxLength: 24, nullable: false),
+                    Fax = table.Column<string>(type: "nvarchar(24)", maxLength: 24, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LastName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    TitleOfCourtesy = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HireDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Adress = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    City = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Region = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    PostalCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    HomePhone = table.Column<string>(type: "nvarchar(24)", maxLength: 24, nullable: false),
+                    Extension = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReportTo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeTerritories",
+                columns: table => new
+                {
+                    TerritoryID = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeTerritories", x => x.TerritoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerID = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    EmployeeID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RequiredDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ShippedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ShipVia = table.Column<int>(type: "int", nullable: true),
+                    Freight = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ShipName = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    ShipAddress = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    ShipCity = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    ShipRegion = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    ShipPostalCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    ShipCountry = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SupplierID = table.Column<int>(type: "int", nullable: false),
+                    CategoryID = table.Column<int>(type: "int", nullable: false),
+                    QuantityPerUnit = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    UnitsInStock = table.Column<int>(type: "int", nullable: false),
+                    UnitsOnOrder = table.Column<int>(type: "int", nullable: false),
+                    ReorderLevel = table.Column<int>(type: "int", nullable: false),
+                    Discontinued = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "CustomerCustomerDemos");
+
+            migrationBuilder.DropTable(
+                name: "CustomerDemographics");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeTerritories");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
--- solutions/18Mayis2022__NorthwindORMMapping/Program.cs
+++ solutions/18Mayis2022__NorthwindORMMapping/Program.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
-
 using System.Diagnostics.CodeAnalysis;
 
 
@@ -14,7 +13,19 @@ public class Northwind : DbContext
 {
     public DbSet<Category> Categories { get; set; }
 
+    public DbSet<Product> Products { get; set; }
+    
+    public DbSet<Customer> Customers { get; set; }
 
+    public DbSet<CustomerDemographic> CustomerDemographics { get; set; }
+    
+    public DbSet<CustomerCustomerDemo> CustomerCustomerDemos { get; set; }
+
+    public DbSet<Employee> Employees { get; set; }
+
+    public DbSet<EmployeeTerritory> EmployeeTerritories { get; set; }
+
+    public DbSet<Order> Orders { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -55,15 +66,11 @@ public class Product
 
     public int ReorderLevel { get; set; }
 
-
     public bool Discontinued { get; set; }
-
-
-
 
 }
 
-public class CUstomer
+public class Customer
 {
     public int id { get; set; }
 
@@ -100,24 +107,68 @@ public class CUstomer
 }
 
 
-
-
-public class CustomerDemographics
+public class CustomerDemographic
 {
     [StringLength(20)]
     public int ID { get; set; }
+
     public string CustomerDesc { get; set; }
 }
-
 
 public class CustomerCustomerDemo
 {
     public int Id { get; set; }
+
     public int CustomerID { get; set; }
+
     public int CustomerTypeID { get; set; }
 }
 
 
+public class Employee
+{
+
+    [Required]
+    public int ID { get; set; }
+    [Required]
+    [StringLength(20)]
+    public string LastName { get; set; }
+    [StringLength(10)]
+    public string FirstName { get; set; }
+    [StringLength(30)]
+    public string Title { get; set; }
+    [StringLength(25)]
+    public string TitleOfCourtesy { get; set; }
+
+    public DateTime BirthDate { get; set; }
+
+    public DateTime HireDate { get; set; }
+    [StringLength(60)]
+
+    public string Adress { get; set; }
+    [StringLength(15)]
+
+    public string City { get; set; }
+    [StringLength(15)]
+
+    public string Region { get; set; }
+    [StringLength(10)]
+
+    public string PostalCode { get; set; }
+
+    [StringLength(15)]
+    public string Country { get; set; }
+
+    [StringLength(24)]
+    public string HomePhone { get; set; }
+
+    [StringLength(4)]
+    public string Extension { get; set; }
+
+    public string Notes { get; set; }
+
+    public int ReportTo { get; set; }
+}
 
 
 public class EmployeeTerritory
@@ -127,10 +178,7 @@ public class EmployeeTerritory
     [StringLength(20)]
     [Key]
     public string TerritoryID { get; set; }
-
-
 }
-
 
 public class Order
 {
@@ -171,39 +219,7 @@ public class Order
 
 }
 
- public class Employee
-    {
-        [Required]
-        public int ID { get; set; }
-        [Required]
-        [StringLength(20)]
-        public string LastName { get; set; }
-        [StringLength(10)]
-        public string FirstName { get; set; }
-        [StringLength(30)]
-        public string Title { get; set; }
-        [StringLength(25)]
-        public string TitleOfCourtesy { get; set; }
-        
-        public DateTime  BirthDate { get; set; }
-        public DateTime HireDate { get; set; }
-        [StringLength(60)]
-        public string Adress { get; set; }
-        [StringLength(15)]
-        public string City { get; set; }
-        [StringLength(15)]
-        public string Region { get; set; }
-        [StringLength(10)]
-        public string PostalCode { get; set; }
-        [StringLength(15)]
-        public string Country { get; set; }
-        [StringLength(24)]
-        public string HomePhone { get; set; }
-        [StringLength(4)]
-        public string Extension { get; set; }
-        public string Notes { get; set; }
-        public int ReportTo { get; set; }
-    }
+
 class program
 {
 
